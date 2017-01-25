253e8d14039931fbb2f7c6aa4a8ace4201d207ee
Blames <nathan.scarsa@gmail.com>
2017-01-25 21:08:51 +0100

    First Commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+
+
+	<div class='footer'>
+		<h5>Copyright &copy; Site blabla E-commerce de mes deux</h5>
+
+
+	</div>
---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+	</head>
+
+
+	<header>
+
+	<br\><h1>SITE DE LA GLOIRE.</h1><br>
+		<ul class="menu">
+			<li><a href="index.php">Acceuil</a></li>
+			<li><a href="boutique.php">Boutique</a></li>
+			<li><a href="panier.php">Panier</a></li>
+			<li><a href="inscription.php">Inscription</a></li>
+			<li><a href="connexion.php">Connexion</a></li>
+		</ul>
+ 
+	</header>
+
+</html>
