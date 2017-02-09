6f9c0cb65f1f8d81ad75e9e9e0b753a2fabac613
Blames <nathan.scarsa@gmail.com>
2017-02-09 09:40:09 +0100

    Boutique updated !

--- boutique.php
+++ boutique.php
@@ -8,21 +8,24 @@ require_once('includes/header.php');
 $db=new PDO('mysql:host=localhost;dbname=icommerce','root','');
 $select = $db->prepare("SELECT * FROM products");
 $select -> execute();
-
+?>
+<div class="row">
+<?php
 while ($s=$select ->fetch (PDO::FETCH_OBJ)){
 	?>
 	
-	<div class="row">
+	
 	  	<div class="col-sm-4">
 			<img src="admin/imgs/<?php echo $s ->name;  ?>.jpg"/>
 			<h2><?php echo $s->name; ?></h2>
 			<h5><?php echo $s->description; ?></h5>
 			<h4><?php echo $s->price; ?>M €</h4>
 		</div>
-	</div>
+
 	<?php
 }
-
-
+?>
+	</div>
+<?php
 require_once('includes/footer.php');
  ?>
